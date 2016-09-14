6a7b5ebbd257629675ac641cfa44963ae180a459
petjelinux <jeffry1829@gmail.com>
2016-09-14 18:35:28 +0800

    make it runable, and modify README

--- README.markdown
+++ README.markdown
@@ -3,5 +3,6 @@
 ```node index.js``` to run.  
 Deploied at port 3000  
 edit /lib/config.js 's path  
+and edit /web/lib/config.js 's domain name as well  
 you needs to put mp3 songs into it  
 that's your song folder
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var config = require('./lib/config.js');
 var express = require('express');
 var app = express();
-var id3 = require('music-tag');
+var jsmediatags = require('jsmediatags');
+var recursive = require('recursive-readdir');
 var mp3duration = require('mp3-duration');
 var path = require('path');
 var fs = require('fs');
@@ -15,7 +16,8 @@ var SongList={};
 var tmp_s_no=0;
 START();
 app.use(bodyParser.json());
-app.use(express.static(songpath));
+app.use(express.static(__dirname+'/web'));
+app.use('/songs',express.static(songpath));
 app.listen(3000);
 app.get('/', function(req, res){
 	res.sendFile(__dirname+'/web/index.html');
@@ -32,38 +34,55 @@ app.post('/getList',function(req, res){
 	res.json(SongList);
 });
 
-function setCurrent(s_id=0, start_time=0){
-	Object.assign(CurrentSong, SongList[s_id]);
+function setCurrent(s_id, start_time){
+	console.log('setCurrent => SongList');
+	console.dir(SongList);
+	
+	if(!s_id){
+		s_id=0;
+	}
+	CurrentSong = Object.assign(CurrentSong, SongList[s_id]);
+	CurrentSong.now_Len = 0;
 	delete CurrentSong['s_path'];
+	
+	console.log('setCurrent => CurrentSong');
+	console.dir(CurrentSong);
 }
 function s_reload(this_f_path){
-	id3.read(this_f_path,{recursive: true}).then(function(result){
-		var p = result.path;
-		p = path.resolve(p);
-		var tags = result.tags;
-		var duration = mp3duration(p); //sec
-		SongList[tags['title']] = {
-				s_path: p,
-				s_name: tags.title,
-				s_id: tmp_s_no++,
-				s_url: '/songs/'+p,
-				s_t: duration,
-				s_description: {
-					artist: tags.artist,
-					album: tags.album
-				}
-		};
-	}).fail(function(err){
-		console.log(err);
-		return;
+	recursive(this_f_path, function(err, files){
+		// file order is not garenteed
+		files.forEach(function(file){
+			file = path.resolve(file);
+			jsmediatags.read(file, {
+				onSuccess: function(result){
+					var tags = result.tags;
+					mp3duration(file, function(err, duration){
+						SongList[tmp_s_no] = {
+								s_path: file,
+								s_name: tags.title, // why is there a "title" tag?!, it's not mentioned in the document!
+								s_id: tmp_s_no++,
+								s_url: '/songs/'+path.basename(file),
+								s_t: duration,
+								s_description: {
+									artist: tags.artist,
+									album: tags.album
+								}
+						};
+					})
+			  },
+			  onError: function(error){
+			    console.log(':(', error.type, error.info);
+			  }
+			})
+		})
 	})
 }
 function START(){
 	s_reload(songpath);
 }
 setInterval(function(){
-	if(CurrentSong.s_id){
-		if(!CurrentSong.now_Len){
+	if(CurrentSong.s_id || CurrentSong.s_id === 0){
+		if(!CurrentSong.now_Len && CurrentSong.now_Len !== 0){
 			CurrentSong.now_Len = 0;
 		}else{
 			if(CurrentSong.s_t - CurrentSong.now_Len > 0){
